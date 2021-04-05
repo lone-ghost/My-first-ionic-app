01d8c4d89749c36fa93bdc0969ce5f80cbd0a52b
Marc Gomis <marcgomis74@gmail.com>
2021-04-05 21:21:43 +0000

    .

---
+++ src/environments/dist/environment.prod.js
@@ -0,0 +1,15 @@
+"use strict";
+exports.__esModule = true;
+exports.environment = void 0;
+exports.environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyDCbSfZtObwsV9d2LBkrz6Q9d1D3aviHbM",
+        authDomain: "foxdev-easy-jobs.firebaseapp.com",
+        databaseURL: "https://foxdev-easy-jobs.firebaseio.com",
+        projectId: "foxdev-easy-jobs",
+        storageBucket: "foxdev-easy-jobs.appspot.com",
+        messagingSenderId: "568185005254",
+        appId: "1:568185005254:web:c8e0e3759efbb0355963c7"
+    }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDCbSfZtObwsV9d2LBkrz6Q9d1D3aviHbM",
+    authDomain: "foxdev-easy-jobs.firebaseapp.com",
+    databaseURL: "https://foxdev-easy-jobs.firebaseio.com",
+    projectId: "foxdev-easy-jobs",
+    storageBucket: "foxdev-easy-jobs.appspot.com",
+    messagingSenderId: "568185005254",
+    appId: "1:568185005254:web:c8e0e3759efbb0355963c7"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
